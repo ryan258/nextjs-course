f32fcfa6b9ecf6516d71ac8b433093355afc146f
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-09 21:32:34 -0500

    Implement a second way of handling <Link /> w/ dynamic values

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -5,9 +5,20 @@ const ClientProjectsPage = () => {
 
   console.log(router.query)
 
+  const loadProjectHandler = () => {
+    // load data - router.push is like using the Link component, just programmatically
+    // router.push('/clients/manny/projectalpha')
+    // or we can pass in an object
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: { id: 'manny', clientprojectid: 'projectalpha' }
+    })
+  }
+
   return (
     <div>
       <h1>Projects For [Client Name Here]</h1>
+      <button onClick={loadProjectHandler}>Load Project Alpha</button>
     </div>
   )
 }
