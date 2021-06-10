99945858940f979affa1c7bab7ff958b243a195d
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-09 21:36:04 -0500

    Set up a custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>Page not found!!!</h1>
+      <p>But good news, here's a ghost... 👻</p>
+    </div>
+  )
+}
+
+export default NotFoundPage
