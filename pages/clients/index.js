990f692a1fe38ba8828d517746dc03943a01b0ee
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-09 21:14:18 -0500

    Implement a second way of handling <Link />

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import Link from 'next/link'
 
 const ClientsPage = () => {
+  const clients = [
+    { id: 'manny', name: 'Manny the Manatee' },
+    { id: 'orson', name: 'Orson the Cow' }
+  ]
+
   return (
     <div>
       <h1>The Clients Page!</h1>
+      <ul>
+        {/* <li>
+          <Link href="/clients/manny">Manny</Link>
+        </li>
+        <li>
+          <Link href="/clients/orson">Orson</Link>
+        </li> */}
+        {clients.map((client) => (
+          <li key={client.id}>
+            {/* <Link href={`/clients/${client.id}`}>{client.name}</Link> */}
+            {/* ALT: vvv instead of a path you can provide an object to your Link */}
+            <Link
+              href={{
+                pathname: '/clients/[id]',
+                query: {
+                  id: client.id
+                }
+              }}
+            >
+              {client.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
